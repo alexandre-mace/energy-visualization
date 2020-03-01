5e642fcf89ae13d2758bdfea8584a97b021c04d5
alexandre-mace <contact.alexandremace@gmail.com>
2020-03-02 00:52:23 +0100

    Updates

---
+++ precache-manifest.00e20c5aed9138386737a2008819c29e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a9e22ccae1c7c9901250992280ba0c",
+    "url": "/energy-visualization/index.html"
+  },
+  {
+    "revision": "96cb5018be81a2108aad",
+    "url": "/energy-visualization/static/css/main.0f1ca384.chunk.css"
+  },
+  {
+    "revision": "2c155e9186014edf1031",
+    "url": "/energy-visualization/static/js/2.52a7ed09.chunk.js"
+  },
+  {
+    "revision": "995084f4dd276fa9cc26db82da00db86",
+    "url": "/energy-visualization/static/js/2.52a7ed09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96cb5018be81a2108aad",
+    "url": "/energy-visualization/static/js/main.ce2d0f0b.chunk.js"
+  },
+  {
+    "revision": "f5bbc14aa27513b83447",
+    "url": "/energy-visualization/static/js/runtime-main.493c6151.js"
+  }
+]);
