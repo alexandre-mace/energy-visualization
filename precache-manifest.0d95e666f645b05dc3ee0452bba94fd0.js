b6a85b41e1130f22da7033a09ab13e154d61af31
alexandre-mace <contact.alexandremace@gmail.com>
2020-03-02 01:37:09 +0100

    Updates

--- precache-manifest.0d95e666f645b05dc3ee0452bba94fd0.js
+++ precache-manifest.0d95e666f645b05dc3ee0452bba94fd0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b1135e534c9e335fd87f8644d3bbdbd1",
+    "revision": "8cdeda1e3f5bb40b9cb39be7d79ffba5",
     "url": "/energy-visualization/index.html"
   },
   {
-    "revision": "b2bf07b9ed18220c45f1",
+    "revision": "def1c6fb8e88b4facfaa",
     "url": "/energy-visualization/static/css/main.0f1ca384.chunk.css"
   },
   {
-    "revision": "c1d17f4d3da662a9c95b",
-    "url": "/energy-visualization/static/js/2.c17dafed.chunk.js"
+    "revision": "311ddedbaf23fafa11bb",
+    "url": "/energy-visualization/static/js/2.5da409aa.chunk.js"
   },
   {
     "revision": "995084f4dd276fa9cc26db82da00db86",
-    "url": "/energy-visualization/static/js/2.c17dafed.chunk.js.LICENSE.txt"
+    "url": "/energy-visualization/static/js/2.5da409aa.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b2bf07b9ed18220c45f1",
-    "url": "/energy-visualization/static/js/main.ad75beb3.chunk.js"
+    "revision": "def1c6fb8e88b4facfaa",
+    "url": "/energy-visualization/static/js/main.2974ad91.chunk.js"
   },
   {
     "revision": "f5bbc14aa27513b83447",
